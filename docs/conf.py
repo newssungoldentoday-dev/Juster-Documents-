028f945b32669c60f2af9a7413651a79456d3079
BlueBixt <newssungoldentoday@gmail.com>
2026-06-14 07:52:17 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,3 +6,11 @@ sys.path.insert(0, os.path.abspath('../System'))
 # Set the version to match your other files
 version = '1.1.0'
 release = '1.1.0'
+
+# --- Static File Configuration ---
+
+# This tells Sphinx to look for your _static folder
+html_static_path = ['_static']
+
+# This tells Sphinx to load your specific stylesheet
+html_css_files = ['style.css']
