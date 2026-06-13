a860bb7320ced80a482115fad410e1d52ebd34cc
BlueBixt <newssungoldentoday@gmail.com>
2026-06-14 06:04:38 +0800

    Create firebase-config.js

---
+++ firebase/firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase configuration for Juster
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "juster-project.firebaseapp.com",
+  projectId: "juster-project",
+  storageBucket: "juster-project.appspot.com",
+  messagingSenderId: "YOUR_ID",
+  appId: "YOUR_APP_ID"
+};
+
+export default firebaseConfig;
