d33de731bed71073b3393ae1b3a837022e52a993
BlueBixt <newssungoldentoday@gmail.com>
2026-06-14 07:32:57 +0800

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,8 @@
+import os
+import sys
+# This tells Sphinx to look inside your System folder
+sys.path.insert(0, os.path.abspath('../System'))
+
+# Set the version to match your other files
+version = '1.1.0'
+release = '1.1.0'
